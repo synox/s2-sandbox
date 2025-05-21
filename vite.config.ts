6c1c38dc5a4b973dd32e1feffd20620cc23bbb81
synox <synox@users.noreply.github.com>
2025-05-21 18:56:59 +0000

    config

--- vite.config.ts
+++ vite.config.ts
@@ -10,31 +10,34 @@
  * governing permissions and limitations under the License.
  */
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import macros from 'unplugin-parcel-macros';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import macros from "unplugin-parcel-macros";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    macros.vite(),
-    react()
-  ],
+  plugins: [macros.vite(), react()],
+  server: {
+    allowedHosts: true,
+  },
   build: {
-    target: ['es2022'],
+    target: ["es2022"],
     // Lightning CSS produces much a smaller CSS bundle than the default minifier.
-    cssMinify: 'lightningcss',
+    cssMinify: "lightningcss",
     rollupOptions: {
       output: {
         // Bundle all S2 and style-macro generated CSS into a single bundle instead of code splitting.
         // Because atomic CSS has so much overlap between components, loading all CSS up front results in
         // smaller bundles instead of producing duplication between pages.
         manualChunks(id) {
-          if (/macro-(.*)\.css$/.test(id) || /@react-spectrum\/s2\/.*\.css$/.test(id)) {
-            return 's2-styles';
+          if (
+            /macro-(.*)\.css$/.test(id) ||
+            /@react-spectrum\/s2\/.*\.css$/.test(id)
+          ) {
+            return "s2-styles";
           }
-        }
-      }
-    }
-  }
-})
+        },
+      },
+    },
+  },
+});
